a166c25c5db716491fa5c4a514d313ef9b7562aa
AjayKumarTyagi <46739062+AjayKumarTyagi@users.noreply.github.com>
2019-03-04 02:04:24 +0530

    Update lab4.1.cpp

--- lab4.1.cpp
+++ lab4.1.cpp
@@ -22,3 +22,25 @@ class student
 		cout<<"roll no: "<<roll<<endl;
 	}	
 };
+class internal:virtual public student
+{
+	protected:
+		int marks1[6],i;
+		public:
+			void  int_marks()
+			{
+				cout<<"internal marks: \n";
+				for(i=0;i<6;i++)
+				{
+					cout<<"enter "<<i+1<<" subject's marks: ";
+					cin>>marks1[i];
+				}
+			}
+			void int_display()
+			{
+				for(i=0;i<6;i++)
+				{
+					cout<<i+1<<" subject's marks: "<<marks1[i]<<endl;
+				}
+			}
+};
