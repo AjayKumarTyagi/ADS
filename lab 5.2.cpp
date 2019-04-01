9e8f6ad8f31da9d2d3043e0a1d390506e6f86ca7
AjayKumarTyagi <46739062+AjayKumarTyagi@users.noreply.github.com>
2019-04-01 19:57:09 +0530

    Update lab 5.2.cpp

--- lab 5.2.cpp
+++ lab 5.2.cpp
@@ -9,4 +9,27 @@ class area
 			side1=x;
 			side2=y;
 		}
+	void choice()
+		{
+		   int c;
+		   int s;
+		   cout<<"1. area of square"<<endl;
+		   cout<<"2. area of rectangle"<<endl;
+		   cout<<"3. area of triangle"<<endl<<endl;
+		   cout<<"enter your choice: ";
+		   cin>>c;
+		   switch(c)
+		   {
+		   	case 1:
+		   	s=calculate(side1);
+			   break;
+			   case 2:
+			  s=calculate(side1,side2);
+			  break;
+			  case 3:
+			  s=calculate(side1,side2);
+			  break;	
+		   }
+		   cout<<s;
+		}
 };    
